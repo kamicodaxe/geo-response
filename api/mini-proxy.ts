72db18c906e91aa3c7b6f07b06d17c8fa66e825a
ruvice101 <kamiruvice@gmail.com>
2020-04-03 23:30:08 +0100

    - small change to test authomatic build

--- api/mini-proxy.ts
+++ api/mini-proxy.ts
@@ -1,6 +1,7 @@
 import { NowRequest, NowResponse } from '@now/node'
 
 module.exports = (req: NowRequest, res: NowResponse) => {
+  console.log('Working!')
   res.json({
     body: req.body,
     query: req.query,
